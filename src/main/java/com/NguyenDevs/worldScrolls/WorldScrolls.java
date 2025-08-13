2014bd7f2feeb9d19d726f7a29e201a1d424fb93
Nguyên Devs <144598460+NguyenDevs@users.noreply.github.com>
2025-08-13 22:33:14 +0700

    Init

---
+++ src/main/java/com/NguyenDevs/worldScrolls/WorldScrolls.java
@@ -0,0 +1,17 @@
+package com.NguyenDevs.worldScrolls;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WorldScrolls extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
